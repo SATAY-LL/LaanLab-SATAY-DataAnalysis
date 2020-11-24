af32a742fe39d02175012aa5351e83f97a2e342b
Gregory94 <g.m.vanbeek@tudelft.nl>
2020-11-24 15:39:44 +0100

    Added demultiplexing.py for own interpretation of the demultiplexing algorithm

---
+++ Python_scripts/demultiplexing.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a python script for demultiplexing reads from transposon sequencing data.
+"""
+
+#import os, sys
+
+
+file1 = r"C:\Users\gregoryvanbeek\Documents\Data_Sets\20201104_Enzo\raw_data_20201104\D18524C717111_BDDP200001534-1A_HJVN5DSXY_L1_1.fq"
+file2 = r"C:\Users\gregoryvanbeek\Documents\Data_Sets\20201104_Enzo\raw_data_20201104\D18524C717111_BDDP200001534-1A_HJVN5DSXY_L1_2.fq"
+
+barcode_forward_sample1 = "GCCACATA"
+barcode_reverse_sample1 = "GCGAGTAA"
+barcode_forward_sample2 = "GAGCTGAA"
+barcode_reverse_sample2 = "GATAGACA"
+
