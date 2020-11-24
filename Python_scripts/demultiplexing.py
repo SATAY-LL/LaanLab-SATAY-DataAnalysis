8b301d1481d0367ad91a6f388297ad2c549006a0
Gregory94 <g.m.vanbeek@tudelft.nl>
2020-11-24 18:38:27 +0100

    Added ability to read the sequencing file, extract the sequences and search for barcodes in the sequences.

--- Python_scripts/demultiplexing.py
+++ Python_scripts/demultiplexing.py
@@ -11,8 +11,33 @@ This is a python script for demultiplexing reads from transposon sequencing data
 file1 = r"C:\Users\gregoryvanbeek\Documents\Data_Sets\20201104_Enzo\raw_data_20201104\D18524C717111_BDDP200001534-1A_HJVN5DSXY_L1_1.fq"
 file2 = r"C:\Users\gregoryvanbeek\Documents\Data_Sets\20201104_Enzo\raw_data_20201104\D18524C717111_BDDP200001534-1A_HJVN5DSXY_L1_2.fq"
 
+
+
 barcode_forward_sample1 = "GCCACATA"
 barcode_reverse_sample1 = "GCGAGTAA"
 barcode_forward_sample2 = "GAGCTGAA"
 barcode_reverse_sample2 = "GATAGACA"
 
+barcode_dict = {barcode_forward_sample1: 'Sample1_for',
+                barcode_reverse_sample1: 'Sample1_rev',
+                barcode_forward_sample2: 'Sample2_for',
+                barcode_reverse_sample2: 'Sample2_rev'}
+
+
+
+f1 = open(file1)
+f2 = open(file2)
+
+
+for line1 in f1:
+    line2 = f2.readline()
+    if line1.startswith('@') and line2.startswith('@') and line1.split(' ')[0] == line2.split(' ')[0]:
+        line1 = f1.readline()
+        bc1 = [barcode for barcode in barcode_dict if barcode in line1]
+    
+        line2 = f2.readline()
+        bc2 = [barcode for barcode in barcode_dict if barcode in line1]
+
+
+f1.close()
+f2.close()
