0ce1b5999bdf40ba170296cda70041145998ae45
Gregory94 <g.m.vanbeek@tudelft.nl>
2020-11-26 08:03:26 +0100

    Fixed bug where there was a newline character too much when writing the reads to the new files.

--- Python_scripts/demultiplexing.py
+++ Python_scripts/demultiplexing.py
@@ -41,78 +41,78 @@ for samplename, outputfile in outputfile_dict.items():
 
 
 #OPEN INPUT FILES
-f1 = open(inputfile_list[0])
-f2 = open(inputfile_list[1])
+f1 = open(inputfile_list[0],'r')
+f2 = open(inputfile_list[1],'r')
 
 
 #CHECK ALL READS IN INPUT FILES FOR BARCODES AND STORE THOSE IN THE DEDICATED OUTPUT FILES.
-#temp=0
+temp=0
 sample_counter = 0
 unmatchedsamples_counter = 0
 nobarcode_counter = 0
 for line1 in f1:
-#    if temp < 10:
-    line2 = f2.readline()
-    if line1.startswith('@') and line2.startswith('@') and line1.split(' ')[0] == line2.split(' ')[0]:
-        line1_header = line1
-        line2_header = line2
-        line1_seqnc = f1.readline()
-        line2_seqnc = f2.readline()
-        line1_dummy = f1.readline()
-        line2_dummy = f2.readline()
-        line1_phred = f1.readline()
-        line2_phred = f2.readline()
-
-#            print(line1_header)
-#            print(line1_seqnc)
-#            print(line1_dummy)
-#            print(line1_phred)
-#            print(line2_header)
-#            print(line2_seqnc)
-#            print(line2_dummy)
-#            print(line2_phred)
-
-        bc1 = [barcode for barcode in barcode_dict if barcode in line1_seqnc]
-#            if not bc1 == []:
-#                print(bc1[0], barcode_dict.get(bc1[0]))
-#            else:
-#                print(bc1)
-
-        bc2 = [barcode for barcode in barcode_dict if barcode in line2_seqnc]
-#            if not bc2 == []:
-#                print(bc2[0], barcode_dict.get(bc2[0]))
-#            else:
-#                print(bc2)
-
-        
-        if not bc1 == [] and not bc2 == []:
-            if barcode_dict.get(bc1[0]).split('_')[0] == barcode_dict.get(bc2[0]).split('_')[0]:
-#                    print(barcode_dict.get(bc1[0]).split('_')[0])
-                with open(outputfile_dict.get(barcode_dict.get(bc1[0])),'a') as f_out:
-                    f_out.write(line1_header + '\n' + line1_seqnc + '\n' + line1_dummy + '\n' + line1_phred)
-                with open(outputfile_dict.get(barcode_dict.get(bc2[0])),'a') as f_out:
-                    f_out.write(line2_header + '\n' + line2_seqnc + '\n' + line2_dummy + '\n' + line2_phred)
-                sample_counter += 1
-
+    if temp < 10:
+        line2 = f2.readline()
+        if line1.startswith('@') and line2.startswith('@') and line1.split(' ')[0] == line2.split(' ')[0]:
+            line1_header = line1.strip('\n')
+            line2_header = line2.strip('\n')
+            line1_seqnc = f1.readline().strip('\n')
+            line2_seqnc = f2.readline().strip('\n')
+            line1_dummy = f1.readline().strip('\n')
+            line2_dummy = f2.readline().strip('\n')
+            line1_phred = f1.readline().strip('\n')
+            line2_phred = f2.readline().strip('\n')
+    
+    #            print(line1_header)
+    #            print(line1_seqnc)
+    #            print(line1_dummy)
+    #            print(line1_phred)
+    #            print(line2_header)
+    #            print(line2_seqnc)
+    #            print(line2_dummy)
+    #            print(line2_phred)
+    
+            bc1 = [barcode for barcode in barcode_dict if barcode in line1_seqnc]
+    #            if not bc1 == []:
+    #                print(bc1[0], barcode_dict.get(bc1[0]))
+    #            else:
+    #                print(bc1)
+    
+            bc2 = [barcode for barcode in barcode_dict if barcode in line2_seqnc]
+    #            if not bc2 == []:
+    #                print(bc2[0], barcode_dict.get(bc2[0]))
+    #            else:
+    #                print(bc2)
+    
+            
+            if not bc1 == [] and not bc2 == []:
+                if barcode_dict.get(bc1[0]).split('_')[0] == barcode_dict.get(bc2[0]).split('_')[0]:
+    #                    print(barcode_dict.get(bc1[0]).split('_')[0])
+                    with open(outputfile_dict.get(barcode_dict.get(bc1[0])),'a') as f_out:
+                        f_out.write(line1_header + '\n' + line1_seqnc + '\n' + line1_dummy + '\n' + line1_phred)
+                    with open(outputfile_dict.get(barcode_dict.get(bc2[0])),'a') as f_out:
+                        f_out.write(line2_header + '\n' + line2_seqnc + '\n' + line2_dummy + '\n' + line2_phred)
+                    sample_counter += 1
+    
+                else:
+    #                    print("Read pair do not belong to same sample")
+                    with open(outputfile_dict.get("unmatched"),'a') as f_out:
+                        f_out.write(line1_header + '\n' + line1_seqnc + '\n' + line1_dummy + '\n' + line1_phred)
+                        f_out.write(line2_header + '\n' + line2_seqnc + '\n' + line2_dummy + '\n' + line2_phred)
+                    unmatchedsamples_counter += 1
             else:
-#                    print("Read pair do not belong to same sample")
+    #                print("Barcode not found in one of the reads")
                 with open(outputfile_dict.get("unmatched"),'a') as f_out:
                     f_out.write(line1_header + '\n' + line1_seqnc + '\n' + line1_dummy + '\n' + line1_phred)
                     f_out.write(line2_header + '\n' + line2_seqnc + '\n' + line2_dummy + '\n' + line2_phred)
-                unmatchedsamples_counter += 1
-        else:
-#                print("Barcode not found in one of the reads")
-            with open(outputfile_dict.get("unmatched"),'a') as f_out:
-                f_out.write(line1_header + '\n' + line1_seqnc + '\n' + line1_dummy + '\n' + line1_phred)
-                f_out.write(line2_header + '\n' + line2_seqnc + '\n' + line2_dummy + '\n' + line2_phred)
-            nobarcode_counter += 1
-
-#            print('')
-#            print('')
-
-#            temp+=1
-#    elif temp >= 10:
-#        break
+                nobarcode_counter += 1
+    
+    #            print('')
+    #            print('')
+    
+            temp+=1
+    elif temp >= 10:
+        break
 
 f1.close()
 f2.close()
