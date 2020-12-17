5ca8b48d569f98848f37450b24912bafd4bfd4cf
Gregory94 <g.m.vanbeek@tudelft.nl>
2020-12-17 12:59:29 +0100

    Added more functionality for checking fastq file format.

--- demultiplexing_scripts/demultiplexing_outputcheck.py
+++ demultiplexing_scripts/demultiplexing_outputcheck.py
@@ -8,6 +8,8 @@ Created on Fri Dec 11 13:46:31 2020
 
 import os, sys
 
+
+
 def check_fastq_pairedinterleaved(inputfile='', barcodes=[]):
 
     if not os.path.isfile(inputfile):
@@ -18,19 +20,44 @@ def check_fastq_pairedinterleaved(inputfile='', barcodes=[]):
         sys.exit()
 
 
-    print("Assessing barcodes: ", barcodes)
+
+    if not barcodes == []:
+        print("Assessing barcodes: ", barcodes)
+
 
 
     total_line_counter = 0
     header_counter = 0
-    pair_counter = 0
-    barcode_in_line = False
     with open(inputfile,'r') as f:
         for line in f:
             total_line_counter += 1
             if line.startswith('@'):
                 header_counter += 1
 
+    print("Number of lines = %i" % total_line_counter)
+    print("Number of reads = %i" % header_counter)
+    lines_divisible_by_four = (total_line_counter / 4).is_integer()
+    if lines_divisible_by_four == False:
+        print("ERROR: Number of lines in fastq file is not a multiple of 4. Format of fastq file may be corrupted.")
+        sys.exit()
+    
+    lines_divisible_by_headercounter = total_line_counter / header_counter
+    if not int(lines_divisible_by_headercounter) == 4:
+        print("ERROR: Number of header in fastq file does not match the number of lines in the file. Format of fastq file may be corrupted.")
+        sys.exit()
+
+
+
+
+    pair_counter = 0
+    with open(inputfile,'r') as f:
+        for line in f:
+            if line.startswith('@'):
+                read_dict = {}
+                read_dict['header'] = line.strip('\n')
+
+
+                #Check if two subsequent headers have the same header id
                 if pair_counter == 0:
                     header1 = line.split(' ')[0]
                     pair_counter += 1
@@ -40,14 +67,28 @@ def check_fastq_pairedinterleaved(inputfile='', barcodes=[]):
                         print('Header %s is not the same as the previous header %s' % (header2, header1))
                     pair_counter = 0
 
+                read_dict['sequence'] = next(f).strip('\n')
+                read_dict['dummy'] = next(f).strip('\n')
+                read_dict['phred'] = next(f).strip('\n')
 
-            if not barcodes == []:
-                if line.startswith('@'):
-                    nextline = next(f) #NOTE: this skips the sequence line in the next loop. 
-                    total_line_counter += 1
 
+                #Check if there are other characters than A,C,T,G or N in the sequence line.
+                valid_char = "ATCGN"
+                if not all(c in valid_char for c in read_dict.get('sequence')):
+                    print("WARNING: Sequence contains invalid characters at header %s " % read_dict.get('header'))
+                elif 'N' in read_dict.get('sequence'):
+                    print("WARNING: Sequence contains N at header %s " % read_dict.get('header'))
+
+
+                #Check if sequence and phred (quality score) have the same number of characters
+                if not len(read_dict.get('sequence')) == len(read_dict.get('phred')):
+                    print("WARNING: sequence and phred lines are not the same length at header %s " % read_dict.get('header'))
+
+
+                #Check if barcodes are present in sequence
+                if not barcodes == []:
                     for barcode in barcodes:
-                        if barcode.upper() in nextline:
+                        if barcode.upper() in read_dict.get('sequence'):
                             barcode_in_line = True
                             break
                     if not barcode_in_line == True:
@@ -55,20 +96,6 @@ def check_fastq_pairedinterleaved(inputfile='', barcodes=[]):
                     else:
                         barcode_in_line = False
 
-
-    print(total_line_counter)
-    print(header_counter)
-    lines_divisible_by_four = (total_line_counter / 4).is_integer()
-    if lines_divisible_by_four == False:
-        print("WARNING: Number of lines in fastq file is not a multiple of 4. Format of fastq file may be corrupted.")
-    
-    lines_divisible_by_headercounter = total_line_counter / header_counter
-    if not int(lines_divisible_by_headercounter) == 4:
-        print("WARNING: Number of header in fastq file does not match the number of lines in the file. Format of fastq file may be corrupted.")
-
-
-#Check if length sequence lines is the same as the length of the phred score lines.
-#Check if every fourth line startswith @
 
 
 
