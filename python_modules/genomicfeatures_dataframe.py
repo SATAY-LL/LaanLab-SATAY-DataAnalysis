e9d00e83d46ac741c61899355ec3513df985146d
Gregory94 <G.M.vanBeek@tudelft.nl>
2021-02-18 10:16:49 +0100

    Updated genomicfeatures_dataframe.py
    Updated help text and changed definition of truncated gene by changing the variable N10percent

--- python_modules/genomicfeatures_dataframe.py
+++ python_modules/genomicfeatures_dataframe.py
@@ -64,7 +64,20 @@ def dna_features(region, wig_file, pergene_insertions_file, variable="reads", pl
         - Verbose: Either True of False. Determines how much textual feedback is given. When set to False, only warnings will be shown.
 
     Output:
-        - dna_df2: Dataframe containing information about the selected chromosome.
+        - dna_df2: Dataframe containing information about the selected chromosome. This includes the following columns:
+            - Feature name
+            - Standard name of the feature
+            - Aliases of feature name (if any)
+            - Feature type (e.g. gene, telomere, centromere, etc. If None, this region is not defined)
+            - Position of feature type in terms of bp relative to chromosome.
+            - Length of region in terms of basepairs
+            - Number of insertions in region
+            - Number of insertions in truncated region where truncated region is the region without the first and last 100bp.
+            - Number of reads in region
+            - Number of reads in truncated region.
+            - Number of reads per insertion (defined by Nreads/Ninsertions)
+            - Number of reads per insertion in truncated region (defined by Nreads_truncatedgene/Ninsertions_truncatedgene)
+            NOTE: truncated regions are only determined for genes. For the other regions the truncated region values are the same as the non-truncated region values.
     
     Required files (see next section):
         - essentials_file: https://github.com/Gregory94/LaanLab-SATAY-DataAnalysis/blob/master/Data_Files/Cerevisiae_AllEssentialGenes_List.txt
@@ -321,7 +334,7 @@ def dna_features(region, wig_file, pergene_insertions_file, variable="reads", pl
             N_reads_list.append(sum(N_reads))
             N_insrt_list.append(len([ins for ins in N_reads if not ins == 0]))
             if not f_type == None and f_type.startswith('Gene'):
-                N10percent = int(len(N_reads) * 0.1) #TRUNCATED GENE DEFINITION
+                N10percent = 100#int(len(N_reads) * 0.1) #TRUNCATED GENE DEFINITION
                 N_reads_truncatedgene_list.append(sum(N_reads[N10percent:-N10percent]))
                 N_insrt_truncatedgene_list.append(len([ins for ins in N_reads[N10percent:-N10percent] if not ins == 0]))
             else:
