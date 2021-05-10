8851fdfd4eb3677782fe1fc12082d5d291c1221b
Floor Dolsma <F.Dolsma@student.tudelft.nl>
2021-05-10 17:32:46 +0200

    real data(from benoit) as input is possible

--- Python_scripts/input_reads_output_fitness.py
+++ Python_scripts/input_reads_output_fitness.py
@@ -5,29 +5,41 @@ Created on Thu May  6 16:15:48 2021
 @author: floor
 """
 import numpy as np
-import random
+#import random
 import math 
 import pandas as pd
+import ast
 
-N=100000000 #number of transposons before sequencing
-m = 50 #different transposons
+
+N=117601914802*10 #number of transposons before sequencing
+ #different transposons
 
 #number of reads for every transposon (ni). 
-#Is now created but would be a list of transposons + #reads
+# Import .txt 
+a = ['variableStep',0]
+df = pd.read_csv(r"C:\Users\floor\OneDrive\Documenten\MASTER\MEP\codes\LaanLab-SATAY-DataAnalysis\Python_scripts\dataWT1.txt", sep = " ")
+df.reset_index(level=0, inplace=True)
+df = df[~df['index'].isin(a)]
+df = df[~df['1'].isin(a)]
+ins_loc = df['1'].tolist()
+ni = df['2'].tolist()
 
-random.seed = 0
-ni = random.sample(range(0, 501), m)
-
+for i in range(0,len(ni)):
+    ni[i] = int(ni[i])
+for i in range(0,len(ni)):
+    ins_loc[i] = int(ins_loc[i])
+    
 #use imput to find n and m
 n = sum(ni) #total number of reads#m = len(ni) #number of different type transposons
+m = len(ins_loc) #number of different transposons
 
 Ni = np.zeros(m)
 Ci = np.zeros(m)
 gf = np.zeros(m)
 fitness = np.zeros(m)
 
-p = 10  #number of PCR cycles
-a = 2**-p
+pcr = 15  #number of PCR cycles
+a = 2**-pcr
 
 
 for i in range (0,m):
@@ -44,5 +56,8 @@ mn = min(gf)
 A = (mx-mn)
 
 for i in range (0,m):
-    fitness [i] = 1- (gf[i]-mn)/A #define fitness
+    fitness [i] = 1- (gf[i]-mn)/A #define fitness ion scale 0-1
 
+ins_fitness = np.column_stack((ins_loc, fitness))
+    
+del (A, mx, mn, a,i)
