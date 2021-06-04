bc01a5b97c9a22564175d07296e039403466aedc
Floor Dolsma <F.Dolsma@student.tudelft.nl>
2021-06-04 17:18:53 +0200

    error bar added

--- Python_scripts/copareWT1_WT2.py
+++ Python_scripts/copareWT1_WT2.py
@@ -11,57 +11,75 @@ import numpy as np
 import csv
 import matplotlib.pyplot as plt
 import pylab
-
+import statistics
+import operator
 #STEP III:
 #compare our fitness values to cellmap data
 #get data from step II and put it in a dataframe
 dataframe = pd.read_csv(r"C:\Users\floor\OneDrive\Documenten\MASTER\MEP\codes\LaanLab-SATAY-DataAnalysis\Python_scripts\ChromosomeRegion_AllGenes.tsv", sep = "\t", names = ["chromosome","gene", "start bp", "stop bp",  "+1 or -1 strand"] )
-dataframe['fitnessWT1'] = ''
+dataframe['readsWT1'] = ''
 dataframe['fitness_avr1'] = ''
 dataframe['fitness_variance1'] = ''
-dataframe['fitness_diff'] = ''
-dataframe['fitnessWT2'] = ''
+dataframe['fitness_variance2'] = ''
+dataframe['readsWT2'] = ''
 dataframe['fitness_avr2'] = ''
+dataframe['error1'] = ''
+dataframe['error2'] = ''
 
 
-dataframe['fitnessWT1']= dataframe['fitnessWT1'].astype('object')
+dataframe['readsWT1']= dataframe['readsWT1'].astype('object')
 
-with open('data_step2WT1.csv', newline='') as f:
+with open('data_step2_readsWT1.csv', newline='') as f:
     reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
     data = list(reader)
-dataframe['fitnessWT1'] = data
+dataframe['readsWT1'] = data
 
-dataframe['fitnessWT2']= dataframe['fitnessWT2'].astype('object')
-with open('data_step2WT2.csv', newline='') as f:
+dataframe['readsWT2']= dataframe['readsWT2'].astype('object')
+
+with open('data_step2_readsWT2.csv', newline='') as f:
     reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
     data2 = list(reader)
-dataframe['fitnessWT2'] = data2
+dataframe['readsWT2'] = data2
 
 
+z = 1.960 #for 95 percent confidence interval
 for i in range(0,len(dataframe)):
-    if (dataframe.at[i,'fitnessWT1']) == []:
-        dataframe.at[i,'fitness_avr1'] =  'no insertions'
+
+
+    if (dataframe.at[i,'readsWT1']) == [] or len(dataframe.at[i, 'readsWT1']) < 2:
+        dataframe.at[i,'fitness_avr1'] =  ''
+       
     else:
-        dataframe.at[i,'fitness_avr1'] = mean(dataframe.at[i,'fitnessWT1'])
-        dataframe.at[i,'fitness_variance1'] = np.var(dataframe.at[i,'fitnessWT1'] )
-    if (dataframe.at[i,'fitnessWT2']) == []:
-        dataframe.at[i,'fitness_avr2'] =  'no insertions'
+        dataframe.at[i,'fitness_avr1'] = statistics.mean(dataframe.at[i,'readsWT1'])
+        dataframe.at[i,'fitness_variance1'] = np.var(dataframe.at[i,'readsWT1'] )
+        dataframe.at[i, 'error1'] = 2*(z*(statistics.stdev(dataframe.at[i, 'readsWT1']))/(np.sqrt(len(dataframe.at[i,'readsWT1']))))
+    
+    if (dataframe.at[i,'readsWT2']) == [] or len(dataframe.at[i, 'readsWT2']) < 2:
+        dataframe.at[i,'fitness_avr2'] =  ''
     else:
-        dataframe.at[i,'fitness_avr2'] = mean(dataframe.at[i,'fitnessWT2'])
-        dataframe.at[i,'fitness_variance2'] = np.var(dataframe.at[i,'fitnessWT2'])
- 
+        dataframe.at[i,'fitness_avr2'] = statistics.mean(dataframe.at[i,'readsWT2'])
+        dataframe.at[i,'fitness_variance2'] = np.var(dataframe.at[i,'readsWT2'])
+        dataframe.at[i, 'error2'] = 2*(z*(statistics.stdev(dataframe.at[i, 'readsWT2']))/(np.sqrt(len(dataframe.at[i,'readsWT2']))))
+
  ## PLOTS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  
 WT1 = []
 WT2 = [] 
-chromosome  = 'chrX'
+err1 = []
+err2 = []
+chromosome  = 'chrXII'
 for i in range(0,len(dataframe)):
-    if  type(dataframe.at[i,'fitness_avr1']) == float and type(dataframe.at[i,'fitness_avr2']) == float and len(dataframe.at[i,'fitnessWT1']) >5 and len(dataframe.at[i,'fitnessWT2']) >5  :
+    if  type(dataframe.at[i,'fitness_avr1']) == float and type(dataframe.at[i,'fitness_avr2']) == float and len(dataframe.at[i,'readsWT1']) >5 and len(dataframe.at[i,'readsWT2']) >5 and (dataframe.at[i,'fitness_avr1']) <500 and (dataframe.at[i,'fitness_avr2']) <500 :
         WT1.append(dataframe.at[i,'fitness_avr1'])
         WT2.append((dataframe.at[i,'fitness_avr2']))
+        err1.append((dataframe.at[i,'error1']))
+        err2.append((dataframe.at[i,'error2']))
         
 plt.style.use('Solarize_Light2')
 plt.plot(WT1, WT2,  'x', color='black')
+
+
+#plt.errorbar(WT1, WT2,  xerr=err1, yerr=err2)
 plt.xlabel('WT1')
 plt.ylabel('WT2')  
 plt.title('Fitness WT1 and WT2' )#( +str(chromosome))
@@ -73,6 +91,6 @@ pylab.plot(WT1,p(WT1),"r--")
 print ("y=%.6fx+(%.6f)"%(z[0],z[1]))
 
 plt.legend()
-plt.savefig('WT1vsWT2.jpg', dpi=1200)
+plt.savefig('afb.jpg', dpi=1200)
 plt.show()
         
