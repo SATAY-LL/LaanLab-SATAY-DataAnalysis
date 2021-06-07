89549bc79bdc4a30686cee1e043a3067bc0b3b6c
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2021-06-07 10:06:55 +0200

    updAte

--- satay/transposonmapping/python_modules/loading_bam_file.py
+++ satay/transposonmapping/python_modules/loading_bam_file.py
@@ -10,6 +10,8 @@ def loading_bam_file(file_path):
         filename = os.path.basename(file_path)
         path = bamfile.replace(filename,'')
 
+## - [] test whether the file has .sorted.bam extension
+## - [] test whether the file exist in the provided location
     assert os.path.isfile(bamfile), 'Bam file not found at: %s' % bamfile #check if given bam file exists
    
     return bamfile
--- tests/test_transposonmaper/test_loading_bam_file.py
+++ tests/test_transposonmaper/test_loading_bam_file.py
@@ -1,14 +1,16 @@
 
 import os
-from satay.transposonmapping.python_modules import loading_bam_file
+from satay.transposonmapping.python_modules.loading_bam_file import loading_bam_file
 
 def test_loading_bam_file():
     
     path = os.path.join('satay/data_files/data_merged_wt/')
     filename = 'WT_merged-techrep-a_techrep-b_trimmed.sorted.bam'
     bamfile = os.path.join(path,filename)
+
+    output=loading_bam_file(file_path=None)
      
-    assert loading_bam_file(file_path=None)==bamfile, "Should be %s" % bamfile
+    assert output==bamfile, "Should be %s" % bamfile
 
     assert loading_bam_file(file_path)==file_path , "Should be %s" % file_path
 
