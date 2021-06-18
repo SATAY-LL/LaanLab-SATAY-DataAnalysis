a3dcb71c1bed53cfdc8c779c77caddf19884e602
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2021-06-18 17:41:32 +0200

    adding test on the format of the output file _pergene.txt

---
+++ tests/test_transposonmaper/test_integration/test_output_format.py
@@ -0,0 +1,33 @@
+import os
+import pkg_resources
+import glob
+import pandas as pd
+
+from satay.transposonmapping.transposonmapping_satay import (
+    transposonmapper 
+)
+
+def test_output_format_pergene():
+    """Test default number of outputs"""
+
+    data_path = pkg_resources.resource_filename("satay", "data_files/files4test")
+    filename = 'SRR062634.filt_trimmed.sorted.bam'
+   
+    filepath = os.path.join(data_path, filename)
+
+    transposonmapper(bamfile=filepath)
+    data=os.listdir(data_path)
+    pergene_file=glob.glob(data_path + '/**/*'+ '_pergene.txt', recursive=True)
+    
+    data=pd.read_csv(pergene_file[0],delimiter='\t')
+    
+    
+    assert data.columns.tolist() == ['Gene name', 
+                                     'Number of transposons per gene',
+                                     'Number of reads per gene'] , "wrong columns names"
+    assert len(data)==6600
+    assert data.iloc[:,0].dtypes=='object'
+    assert data.iloc[:,1].dtypes=='int64'
+    assert data.iloc[:,2].dtypes=='int64'
+    
+    
