58a28e758b86094273324f09ee5b8da19e9dfc0d
Maurits Kok <m.w.a.kok@tudelft.nl>
2021-06-23 13:21:23 +0200

    Move read genes to importing folder

--- satay/transposonmapping/importing/__init__.py
+++ satay/transposonmapping/importing/__init__.py
@@ -1,2 +1,3 @@
 from .verify_data_files import verify_data_files
 from .load_default_files import load_default_files
+from .read_genes import read_genes
--- satay/transposonmapping/python_modules/__init__.py
+++ satay/transposonmapping/python_modules/__init__.py
@@ -2,25 +2,27 @@ from .loading_files import access_files
 from .chromosome_and_gene_positions import chromosomename_roman_to_arabic, gene_position
 from .gene_names import gene_aliases
 from .samflag import samflags
-from .get_chromosome_properties import get_chromosome_names, get_sequence_length, get_chromosome_reads
+from .get_chromosome_properties import (
+    get_chromosome_names,
+    get_sequence_length,
+    get_chromosome_reads,
+)
 from .get_reads import get_reads
-from .read_genes import read_genes
 from .concatenate_chromosomes import add_chromosome_length
 from .concatenate_chromosomes import add_chromosome_length_inserts
 from .get_insertions_and_reads import get_insertions_and_reads
 
 __all__ = [
-    access_files, 
+    access_files,
     add_chromosome_length,
     add_chromosome_length_inserts,
-    chromosomename_roman_to_arabic, 
-    gene_aliases, 
-    gene_position, 
+    chromosomename_roman_to_arabic,
+    gene_aliases,
+    gene_position,
     get_chromosome_names,
     get_chromosome_reads,
     get_insertions_and_reads,
     get_reads,
     get_sequence_length,
-    read_genes,
-    samflags, 
-    ]
+    samflags,
+]
