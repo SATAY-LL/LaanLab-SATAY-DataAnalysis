4e0fb17112da1ef6959072860b13808447db13f6
Maurits Kok <m.w.a.kok@tudelft.nl>
2021-06-23 13:17:28 +0200

    Remove print output bed file

--- satay/transposonmapping/exporting/save_as_bed.py
+++ satay/transposonmapping/exporting/save_as_bed.py
@@ -1,26 +1,31 @@
 import os
 
-def save_as_bed(bamfile, tncoordinates_array, ref_tid, readnumb_array):
+
+def save_as_bed(file, tncoordinates_array, ref_tid, readnumb_array):
     """
 
     """
 
+    with open(file, "w") as f:
 
-    bedfile = bamfile + '.bed'    
-    filename = os.path.basename(bamfile)
+        base = os.path.basename(file)
+        filename = os.path.splitext(base)[0]
+        f.write("track name=" + filename + " useScore=1\n")
 
-    print('Writing bed file at: ', bedfile)
-    print('')
-
-
-    with open(bedfile, 'w') as f:
-        
-        f.write('track name=' + filename + ' useScore=1\n')
-        
         coordinates_counter = 0
         for tn in tncoordinates_array:
             refname = [key for key, val in ref_tid.items() if val == tn[0] - 1][0]
-            if refname == 'Mito':
-                refname = 'M'
-            f.write('chr' + refname + ' ' + str(tn[1]) + ' ' + str(tn[1] + 1) + ' . ' + str(100+readnumb_array[coordinates_counter]*20) + '\n')
+            if refname == "Mito":
+                refname = "M"
+            f.write(
+                "chr"
+                + refname
+                + " "
+                + str(tn[1])
+                + " "
+                + str(tn[1] + 1)
+                + " . "
+                + str(100 + readnumb_array[coordinates_counter] * 20)
+                + "\n"
+            )
             coordinates_counter += 1
