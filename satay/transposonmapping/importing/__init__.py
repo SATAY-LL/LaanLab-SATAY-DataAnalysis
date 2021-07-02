be5a45e24f21001ffd7ed2bed794269b94ae325d
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2021-07-02 12:14:02 +0200

    adding load_sgd_tabs

--- satay/transposonmapping/importing/__init__.py
+++ satay/transposonmapping/importing/__init__.py
@@ -1,9 +1,10 @@
 from .verify_data_files import verify_data_files
-from .load_default_files import load_default_files
+from .load_default_files import load_default_files,load_sgd_tab
 from .read_genes import read_genes
 from .loading_files import access_files
 
 __all__ = [
     "load_default_files",
     "read_genes",
+    "load_sgd_tab"
 ]
