bc746dbc30f9a8a7c4251965ae2ea12dff087f1c
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2021-07-02 12:13:18 +0200

    adding loading of sgd tabs file

--- satay/transposonmapping/importing/load_default_files.py
+++ satay/transposonmapping/importing/load_default_files.py
@@ -1,6 +1,4 @@
 import os
-
-import os
 import pkg_resources
 
 
@@ -21,4 +19,18 @@ def load_default_files(gff_file, essentials_file, gene_names_file):
     if gene_names_file is None:
         gene_names_file = os.path.join(default_path, "Yeast_Protein_Names.txt")
 
+
     return gff_file, essentials_file, gene_names_file
+
+
+def load_sgd_tab(sgd_features_file):
+    """
+    
+    """
+    default_path = pkg_resources.resource_filename("satay", "data_files/")
+    if sgd_features_file is None: 
+
+        sgd_features_file = os.path.join(default_path,'SGD_features.tab')
+
+    return sgd_features_file
+
