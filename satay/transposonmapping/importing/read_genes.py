97674c59a4da6af443147605a1d1cdf9533f41e8
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2021-07-07 11:12:07 +0200

    docstring added

--- satay/transposonmapping/importing/read_genes.py
+++ satay/transposonmapping/importing/read_genes.py
@@ -1,12 +1,34 @@
-from ..properties.get_gene_position import gene_position
-from ..properties.gene_aliases import gene_aliases
+from satay.transposonmapping.properties.get_gene_position import gene_position
+from satay.transposonmapping.properties.gene_aliases import gene_aliases
 
 
 def read_genes(gff_file, essentials_file, gene_names_file):
     """
+    This function reads the useful information inside the gff_file, essentials_file and
+    gene_names_file. 
+    For the gff_file and essentials_file extracts the gene coordinates , specifying the chromosome, start ,end 
+    and direction. 
+    For the gene_names_files it translates the systematic name into the standard name.
+    
+    Usage: 
 
-    Syntax: [gene_coordinates, essential_coordinates, aliases_designation] = 
+     [gene_coordinates, essential_coordinates, aliases_designation] = 
             read_genes(gff_file, essentials_file, gene_names_file)
+    
+    Inputs:
+        - gff_file : Annotated genome from Saccharomyces cerevisiae (baker's yeast) (https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_genomic.gff.gz)
+        - essentials_file:  Essentials genes annotated from yeast , written using the systematic name standard , all in one column
+        - gene_names_file:  This documents lists all the Saccharomyces cerevisiae S288c entries present in
+this release of UniProtKB/Swiss-Prot. 
+            Yeast (Saccharomyces cerevisiae): entries, gene names and cross-references to SGD. 
+             Release:     2021_01 of 10-Feb-2021
+            
+    Outputs:
+        gene_coordinates : a dict specifying for each gene the chromosome number the gene
+        belongs to, the start gene coordinate, the end gene coordinate and the strand direction ('+' or '-'). 
+        essential_coordinates: a dict specifying for each annotated essential gene the chromosome number the gene
+        belongs to, the start gene coordinate, the end gene coordinate and the strand direction ('+' or '-'). 
+        aliases_designation: a dict that for each systematic gene name specify the standard gene name.  
 
     """
 
